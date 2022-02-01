aa0c0e8b1bcb0b0b87e53791928175cee03974be
Ayush <>
2022-02-01 21:24:35 +0530

    updated script rust nightly version

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,8 +5,8 @@ set -e
 echo "*** Initializing WASM build environment"
 
 rustup update stable
-rustup update nightly
-rustup toolchain install nightly
-rustup target add wasm32-unknown-unknown --toolchain nightly
-rustup default nightly
-rustup override set nightly
+rustup update nightly-2021-11-07
+rustup toolchain install nightly-2021-11-07
+rustup target add wasm32-unknown-unknown --toolchain nightly-2021-11-07
+rustup default nightly-2021-11-07
+rustup override set nightly-2021-11-07

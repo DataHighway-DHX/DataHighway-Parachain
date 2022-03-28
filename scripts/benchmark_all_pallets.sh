a3aaece98682da9dd653e3a4591f0f5916602337
Luke Schoen <ltfschoen@gmail.com>
2022-03-28 10:46:48 +0200

    wip

--- scripts/benchmark_all_pallets.sh
+++ scripts/benchmark_all_pallets.sh
@@ -33,6 +33,37 @@ pallets=(
     "pallet_indices" \
     "pallet_transaction_payment" \
     "pallet_xcm" \
+    "membership_supernodes" \
+    "roaming_operators" \
+    "roaming_networks" \
+    "roaming_organizations" \
+    "roaming_network_servers" \
+    "roaming_devices" \
+    "roaming_routing_profiles" \
+    "roaming_service_profiles" \
+    "roaming_accounting_policies" \
+    "roaming_agreement_policies" \
+    "roaming_network_profiles" \
+    "roaming_device_profiles" \
+    "roaming_sessions" \
+    "roaming_billing_policies" \
+    "roaming_charging_policies" \
+    "roaming_packet_bundles" \
+    "mining_setting_token" \
+    "mining_setting_hardware" \
+    "mining_rates_token" \
+    "mining_rates_hardware" \
+    "mining_sampling_token" \
+    "mining_sampling_hardware" \
+    "mining_eligibility_token" \
+    "mining_eligibility_hardware" \
+    "mining_eligibility_proxy" \
+    "mining_lodgements_hardware" \
+    "mining_claims_token" \
+    "mining_claims_hardware" \
+    "mining_execution_token" \
+    "exchange_rate" \
+    "treasury_dao" \
 )
 
 echo building runtime-benchmarking feature...

58b9cd5d51a823614afc073ccae6ac0496d8d141
sudipghimire533 <sudipghimire533@gmail.com>
2022-08-15 17:33:54 +0545

    Update runtime version (spec_version:) from 4 to 5

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -237,7 +237,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("datahighway-parachain"),
     impl_name: create_runtime_str!("datahighway-parachain"),
     authoring_version: 2,
-    spec_version: 4,
+    spec_version: 5,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,

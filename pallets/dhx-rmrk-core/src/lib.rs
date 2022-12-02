8df1958140a8da532c4cc43da5c22b1d38631732
sudipghimire533 <sudipghimire533@gmail.com>
2022-12-02 09:34:04 +0545

    wrap mint_directly & create_collection extrinsic

--- pallets/dhx-rmrk-core/src/lib.rs
+++ pallets/dhx-rmrk-core/src/lib.rs
@@ -17,6 +17,7 @@ pub mod pallet {
     use frame_system::pallet_prelude::*;
     use frame_support::{transactional, sp_runtime::Permill};
     use pallet_rmrk_core::BoundedResourceInfoTypeOf;
+    use pallet_rmrk_core::BoundedCollectionSymbolOf;
     use pallet_rmrk_core::WeightInfo;
 
     type NftId<T> = <T as pallet_uniques::pallet::Config>::ItemId;
@@ -82,6 +83,58 @@ pub mod pallet {
                 transferable,
                 resources
             )
+        }
+
+        /// Mints an NFT in the specified collection directly to another NFT
+		/// Sets metadata and the royalty attribute
+		///
+		/// Parameters:
+		/// - `collection_id`: The class of the asset to be minted.
+		/// - `nft_id`: The nft value of the asset to be minted.
+		/// - `recipient`: Receiver of the royalty
+		/// - `royalty`: Permillage reward from each trade for the Recipient
+		/// - `metadata`: Arbitrary data about an nft, e.g. IPFS hash
+		#[pallet::weight(<T as pallet_rmrk_core::Config>::WeightInfo::mint_nft_directly_to_nft())]
+		#[transactional]
+		pub fn mint_nft_directly_to_nft(
+			origin: OriginFor<T>,
+			owner: (T::CollectionId, T::ItemId),
+			nft_id: T::ItemId,
+			collection_id: T::CollectionId,
+			royalty_recipient: Option<T::AccountId>,
+			royalty: Option<Permill>,
+			metadata: BoundedVec<u8, T::StringLimit>,
+			transferable: bool,
+			resources: Option<BoundedResourceInfoTypeOf<T>>,
+		) -> DispatchResult {
+            let _allowed_minter: T::AccountId = T::ProducerOrigin::ensure_origin(origin.clone())
+                .map_err(|_| Error::<T>::InsufficientPermission)?;
+            pallet_rmrk_core::Pallet::<T>::mint_nft_directly_to_nft(
+                origin,
+                owner,
+                nft_id,
+                collection_id,
+                royalty_recipient,
+                royalty,
+                metadata,
+                transferable,
+                resources
+            )
+        }
+
+        /// Create a collection
+		#[pallet::weight(<T as pallet_rmrk_core::Config>::WeightInfo::create_collection())]
+		#[transactional]
+		pub fn create_collection(
+			origin: OriginFor<T>,
+			collection_id: T::CollectionId,
+			metadata: BoundedVec<u8, T::StringLimit>,
+			max: Option<u32>,
+			symbol: BoundedCollectionSymbolOf<T>,
+		) -> DispatchResult {
+            let _allowed_minter: T::AccountId = T::ProducerOrigin::ensure_origin(origin.clone())
+                .map_err(|_| Error::<T>::InsufficientPermission)?;
+            pallet_rmrk_core::Pallet::<T>::create_collection(origin, collection_id, metadata, max, symbol)
         }
 	}
 }

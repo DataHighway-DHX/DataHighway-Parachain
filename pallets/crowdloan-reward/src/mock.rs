dbd74015c4077579c3b5ba381e139041d7e56261
Sudip Ghimire <sudipghimire533@gmail.com>
2023-02-10 16:50:00 +0100

    Add sample complete flow test

--- pallets/crowdloan-reward/src/mock.rs
+++ pallets/crowdloan-reward/src/mock.rs
@@ -1,10 +1,20 @@
 use crate as crowdloan_reward;
-use frame_support::{traits::{ConstU16, ConstU64}, parameter_types};
+use crate::types;
+use frame_support::{
+    parameter_types,
+    traits::{
+        ConstU16,
+        ConstU64,
+    },
+};
 use frame_system as system;
 use sp_core::H256;
 use sp_runtime::{
-	testing::Header,
-	traits::{BlakeTwo256, IdentityLookup},
+    testing::Header,
+    traits::{
+        BlakeTwo256,
+        IdentityLookup,
+    },
 };
 
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
@@ -14,81 +24,97 @@ type AccountId = u64;
 type BlockNumber = u64;
 
 frame_support::construct_runtime!(
-	pub enum Test where
-		Block = Block,
-		NodeBlock = Block,
-		UncheckedExtrinsic = UncheckedExtrinsic,
-	{
-		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+    pub enum Test where
+        Block = Block,
+        NodeBlock = Block,
+        UncheckedExtrinsic = UncheckedExtrinsic,
+    {
+        System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
         Balances: pallet_balances::{Pallet, Call, Storage, Event<T>},
         Vesting: pallet_vesting::{Pallet, Call, Storage, Event<T>, Config<T>},
         Reward: crowdloan_reward::{Pallet, Call, Storage, Event<T> },
-	}
+    }
 );
 
 impl system::Config for Test {
-	type BaseCallFilter = frame_support::traits::Everything;
-	type BlockWeights = ();
-	type BlockLength = ();
-	type DbWeight = ();
-	type Origin = Origin;
-	type Call = Call;
-	type Index = u64;
-	type BlockNumber = BlockNumber;
-	type Hash = H256;
-	type Hashing = BlakeTwo256;
-	type AccountId = AccountId;
-	type Lookup = IdentityLookup<Self::AccountId>;
-	type Header = Header;
-	type Event = Event;
-	type BlockHashCount = ConstU64<250>;
-	type Version = ();
-	type PalletInfo = PalletInfo;
-	type AccountData = pallet_balances::AccountData<Balance>;
-	type OnNewAccount = ();
-	type OnKilledAccount = ();
-	type SystemWeightInfo = ();
-	type SS58Prefix = ConstU16<42>;
-	type OnSetCode = ();
-	type MaxConsumers = frame_support::traits::ConstU32<16>;
+    type AccountData = pallet_balances::AccountData<Balance>;
+    type AccountId = AccountId;
+    type BaseCallFilter = frame_support::traits::Everything;
+    type BlockHashCount = ConstU64<250>;
+    type BlockLength = ();
+    type BlockNumber = BlockNumber;
+    type BlockWeights = ();
+    type Call = Call;
+    type DbWeight = ();
+    type Event = Event;
+    type Hash = H256;
+    type Hashing = BlakeTwo256;
+    type Header = Header;
+    type Index = u64;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type MaxConsumers = frame_support::traits::ConstU32<16>;
+    type OnKilledAccount = ();
+    type OnNewAccount = ();
+    type OnSetCode = ();
+    type Origin = Origin;
+    type PalletInfo = PalletInfo;
+    type SS58Prefix = ConstU16<42>;
+    type SystemWeightInfo = ();
+    type Version = ();
 }
 
 parameter_types! {
-	pub const ExistentialDeposit: u128 = 500;
-	pub const MaxLocks: u32 = 50;
-	pub const VestingMinTransfer: Balance = 1000;
+    pub const ExistentialDeposit: u128 = 500;
+    pub const MaxLocks: u32 = 50;
+    pub const VestingMinTransfer: Balance = 1000;
 }
 
 impl pallet_balances::Config for Test {
- 	type Balance = Balance;
- 	type DustRemoval = ();
- 	type Event = Event;
- 	type ExistentialDeposit = ExistentialDeposit;
- 	type AccountStore = System;
- 	type WeightInfo = ();
- 	type MaxLocks = MaxLocks;
- 	type MaxReserves = ();
- 	type ReserveIdentifier = [u8; 8];
+    type AccountStore = System;
+    type Balance = Balance;
+    type DustRemoval = ();
+    type Event = Event;
+    type ExistentialDeposit = ExistentialDeposit;
+    type MaxLocks = MaxLocks;
+    type MaxReserves = ();
+    type ReserveIdentifier = [u8; 8];
+    type WeightInfo = ();
 }
 
 impl pallet_vesting::Config for Test {
-    type Event = Event;
-    type Currency = Balances;
     type BlockNumberToBalance = sp_runtime::traits::ConvertInto;
+    type Currency = Balances;
+    type Event = Event;
     type MinVestedTransfer = VestingMinTransfer;
     type WeightInfo = ();
-    const MAX_VESTING_SCHEDULES:u32 = 20;
+
+    const MAX_VESTING_SCHEDULES: u32 = 20;
 }
 
 impl crowdloan_reward::Config for Test {
-	type Event = Event;
-    type Currency = Balances;
     type BlockNumberToBalance = sp_runtime::traits::ConvertInto;
     type CrowdloanId = u32;
+    type Currency = Balances;
     type CurrencyConvert = sp_runtime::traits::ConvertInto;
+    type Event = Event;
 }
 
 // Build genesis storage according to the mock runtime.
 pub fn new_test_ext() -> sp_io::TestExternalities {
-	system::GenesisConfig::default().build_storage::<Test>().unwrap().into()
+    system::GenesisConfig::default().build_storage::<Test>().unwrap().into()
+}
+
+pub fn run_to_block(n: types::BlockNumberOf<Test>) {
+    use frame_support::traits::Hooks;
+
+    while System::block_number() < n {
+        if System::block_number() > 1 {
+            Reward::on_finalize(System::block_number());
+            System::on_finalize(System::block_number());
+        }
+        System::set_block_number(System::block_number() + 1);
+
+        System::on_initialize(System::block_number());
+        Reward::on_initialize(System::block_number());
+    }
 }
--- pallets/crowdloan-reward/src/tests.rs
+++ pallets/crowdloan-reward/src/tests.rs
@@ -1,1 +1,116 @@
+use crate::{
+    functions,
+    mock::*,
+    types,
+    Error,
+};
+use frame_support::{
+    assert_noop,
+    assert_ok,
+};
 
+#[test]
+fn campaign_creation_success() {
+    new_test_ext().execute_with(|| {
+        // move the current block number
+        let current_block_num = 5;
+        run_to_block(current_block_num);
+
+        // initilization data for a crowdloan campaign
+        let reward_source = 1u32.into();
+        let crowdloan_id = 3u32;
+        let hoster = 10u32.into();
+        let reward_pool = 10_000_000;
+        let info = types::CrowdloanRewardParamFor::<Test> {
+            hoster: None,
+            reward_source: Some(reward_source),
+            total_pool: Some(Some(reward_pool)),
+            instant_percentage: Some(types::SmallRational {
+                numenator: 3,
+                denomator: 10,
+            }),
+            starts_from: None,
+            end_target: Some(100u32.into()),
+        };
+
+        // Create crowdloan
+        assert_ok!(Reward::start_new_crowdloan(Origin::signed(hoster), crowdloan_id, info.clone()));
+        // Make sure status is `InProgress`
+        assert_eq!(Reward::get_campaign_status(&crowdloan_id), Some(types::RewardCampaignStatus::InProgress));
+        // Also check the reward details is filled correctly
+        assert_eq!(
+            Reward::get_reward_info(&crowdloan_id),
+            Some(types::CrowdloanRewardFor::<Test> {
+                hoster,
+                starts_from: current_block_num,
+                total_pool: Some(reward_pool),
+                instant_percentage: types::SmallRational {
+                    numenator: 3,
+                    denomator: 10
+                },
+                end_target: 100u32.into(),
+                reward_source,
+            })
+        );
+
+        // Data of a usual contributer
+        let user_a = (11u32.into(), 1_000_000_u128);
+        let user_b = (12u32.into(), 2_100_004_u128);
+        let user_a_reward = types::RewardUnitOf::<Test> {
+            instant_amount: 300_000 + 40,
+            vesting_amount: 700_000,
+            per_block: 7368,
+            status: types::ClaimerStatus::Unprocessed,
+        };
+        let user_b_reward = types::RewardUnitOf::<Test> {
+            instant_amount: 6300012 + 13,
+            vesting_amount: 14700028,
+            per_block: 154737,
+            status: types::ClaimerStatus::Unprocessed,
+        };
+
+        assert_eq!(user_a_reward.instant_amount + user_a_reward.vesting_amount, user_a.1);
+        assert_eq!(user_b_reward.instant_amount + user_b_reward.vesting_amount, user_b.1);
+
+        // add users
+        assert_ok!(Reward::add_contributer(Origin::signed(hoster), crowdloan_id, user_a.0, user_a.1));
+        assert_ok!(Reward::add_contributer(Origin::signed(hoster), crowdloan_id, user_b.0, user_b.1));
+
+        // Check contributers are added properly
+        assert_eq!(Reward::get_contribution(crowdloan_id, user_a.0).as_ref(), Some(&user_a_reward));
+        assert_eq!(Reward::get_contribution(crowdloan_id, user_b.0).as_ref(), Some(&user_b_reward));
+
+        // lock campaign
+        assert_ok!(Reward::lock_campaign(Origin::signed(hoster), crowdloan_id));
+        // check lock status
+        assert_eq!(Reward::get_campaign_status(crowdloan_id), Some(types::RewardCampaignStatus::Locked));
+
+        // now user can claim the reward
+        assert_ok!(Reward::get_instant_reward(Origin::signed(user_a.0), crowdloan_id));
+        assert_ok!(Reward::get_vested_reward(Origin::signed(user_b.0), crowdloan_id));
+
+        // Make sure only status is updated
+        assert_eq!(Reward::get_contribution(crowdloan_id, user_a.0), Some(types::RewardUnitOf::<Test> {
+            status: types::ClaimerStatus::DoneInstant,
+            ..user_a_reward.clone()
+        }));
+        assert_eq!(Reward::get_contribution(crowdloan_id, user_b.0), Some(types::RewardUnitOf::<Test> {
+            status: types::ClaimerStatus::DoneVesting,
+            ..user_b_reward.clone()
+        }));
+
+        // now user can claim the reward
+        assert_ok!(Reward::get_vested_reward(Origin::signed(user_a.0), crowdloan_id));
+        assert_ok!(Reward::get_instant_reward(Origin::signed(user_b.0), crowdloan_id));
+
+        // Again status should be updated
+        assert_eq!(Reward::get_contribution(crowdloan_id, user_a.0), Some(types::RewardUnitOf::<Test> {
+            status: types::ClaimerStatus::DoneBoth,
+            ..user_a_reward
+        }));
+        assert_eq!(Reward::get_contribution(crowdloan_id, user_b.0), Some(types::RewardUnitOf::<Test> {
+            status: types::ClaimerStatus::DoneBoth,
+            ..user_b_reward
+        }));
+    });
+}

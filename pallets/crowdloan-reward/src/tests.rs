2c26cc899cd5592f7a9d7a3f733b4b95fd37761b
Sudip Ghimire <sudipghimire533@gmail.com>
2023-02-17 13:53:46 +0100

    Add test for lock_campaign success

--- pallets/crowdloan-reward/src/tests.rs
+++ pallets/crowdloan-reward/src/tests.rs
@@ -12,6 +12,7 @@ use frame_support::{
     assert_ok,
     assert_storage_noop,
     inherent::BlockT,
+    IterableStorageDoubleMap,
 };
 use frame_system::RawOrigin;
 
@@ -651,5 +652,39 @@ fn discard_campaign_success() {
         assert_eq!(crate::Contribution::<Test>::iter_prefix(crowdloan_id).next(), None);
         // should be event
         assert_eq!(reward_events().last(), Some(&RewardEvent::CampaignDiscarded(crowdloan_id)));
+    });
+}
+
+#[test]
+fn lock_campaign_success() {
+    new_test_ext().execute_with(|| {
+        let hoster = 1_u32.into();
+        let crowdloan_id = 33_u32.into();
+
+        // initilization
+        assert_ok!(Reward::start_new_crowdloan(
+            Origin::signed(hoster),
+            crowdloan_id,
+            types::CrowdloanRewardParamFor::<Test> {
+                hoster: None,
+                reward_source: Some(100_u32.into()),
+                instant_percentage: Some(types::SmallRational::new(3, 10)),
+                starts_from: Some(0_u32.into()),
+                end_target: Some(100_u32.into()),
+            }
+        ));
+
+        // add contributers
+        assert_ok!(Reward::add_contributer(Origin::signed(hoster), crowdloan_id, 101_u32.into(), 100_000));
+        assert_ok!(Reward::add_contributer(Origin::signed(hoster), crowdloan_id, 102_u32.into(), 100_000));
+
+        // lock the campaign
+        assert_ok!(Reward::lock_campaign(Origin::signed(hoster), crowdloan_id));
+        // status should be locked
+        assert_eq!(Reward::get_campaign_status(crowdloan_id), Some(types::RewardCampaignStatus::Locked));
+        // contributer should be as-is
+        assert_eq!(crate::Contribution::<Test>::iter_key_prefix(crowdloan_id).collect::<Vec<_>>(), vec![102, 101]);
+        // event must be deposited
+        assert_eq!(reward_events().last(), Some(&RewardEvent::CampaignLocked(crowdloan_id)));
     });
 }

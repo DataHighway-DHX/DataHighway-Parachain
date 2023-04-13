a6d2b50200418f66fc0908bffe5f16aa11469d76
Branimir Malesevic <glbranimir@gmail.com>
2023-04-13 13:03:11 +0200

    Set treasury proposal minimum bond to 10k DHX (#62)
    * Increase treasury minimum to 10k DHX
    
    * Bump versions

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -227,7 +227,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     impl_name: create_runtime_str!("datahighway-parachain"),
     authoring_version: 2,
     spec_version: 9,
-    impl_version: 0,
+    impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
 	state_version: 1,
@@ -504,7 +504,7 @@ impl pallet_membership::Config<pallet_membership::Instance1> for Runtime {
 
 parameter_types! {
     pub const ProposalBond: Permill = Permill::from_percent(5);
-    pub const ProposalBondMinimum: Balance = 1 * DOLLARS;
+    pub const ProposalBondMinimum: Balance = 10_000 * DOLLARS;
     pub const SpendPeriod: BlockNumber = 1 * DAYS;
     pub const Burn: Permill = Permill::from_percent(0);
     pub const TipCountdown: BlockNumber = 1 * DAYS;
